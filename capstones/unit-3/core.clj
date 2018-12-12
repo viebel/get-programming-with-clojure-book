abeb6915076058bbdf98d8057631e396e7f0c319
Yehonathan Sharvit <viebel@gmail.com>
2018-12-12 07:40:24 +0200

    Update core.clj

--- capstones/unit-3/core.clj
+++ capstones/unit-3/core.clj
@@ -1,10 +1,12 @@
 ;; total amount 
-;; each item costs $10
+;; shipping costs $10
 ;; discount based on number of items purchased
 ;; below 5 items - no discount
 ;; between 6 and 10 items - 15 % discount
 ;; between 11 and 20 items - 20 % discount
 ;; above 20 items - 30 % discount
+
+(def shipping-cost 10)
 
 (defn discount [num-of-items]
   (cond 
@@ -16,8 +18,10 @@
 (defn reducted-price-ratio [num-of-items]
   (- 1 (/ (discount num-of-items)
           100)))
-  
-(defn total-amount [num-of-items item-price]
-  (* num-of-items item-price (reducted-price-ratio num-of-items)))
 
-(total-amount 6 9)
+(defn total-amount [num-of-items item-price shipping?]
+  (if shipping?
+    (+ shipping-cost (* num-of-items item-price (reducted-price-ratio num-of-items)))
+    (* num-of-items item-price (reducted-price-ratio num-of-items))))
+
+(total-amount 6 9 false)
