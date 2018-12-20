1be091c9240fb37bd0334356d4b9923b1bf2b8f9
Yehonathan Sharvit <viebel@gmail.com>
2018-12-21 00:08:29 +0200

    Update winner-beers.clj

--- capstones/unit-3/winner-beers.clj
+++ capstones/unit-3/winner-beers.clj
@@ -1,3 +1,13 @@
+; An american, a british and a french are sitting at a pub. After a couple of hours, each of them brags about the amount of beer he has drunk:
+; The american guy says: "I have drunk x 20 ounce cans"
+; The british guy replies: "I have drunk y pints"
+; The french guy concludes: "I have drunk demis"
+
+; Which one of them has drunk the most?
+
+; Your mission is to write a function calc-most-drunk that receive three arguments x, y and z and returns a number corresponding to the guy that drank the most: 1 for the american, 2 for the british, 3 for the french.
+; Bonus: return a string with the name of the winner and the qunatity of beers he drunk
+
 (defn us->cl [num] num)
 (defn en->cl [num] num)
 (defn fr->cl [num] num)
